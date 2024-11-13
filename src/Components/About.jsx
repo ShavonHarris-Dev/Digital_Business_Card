ee85e1a359f48dbf45cc2c4609c89f800d749990
Shavon Harris <Shavonharris9114@gmail.com>
2024-11-12 20:28:36 -0600

    chore: add resume link

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,7 +4,6 @@ export default function About() {
             <h2>About </h2>
             <p>
             I&rsquo;m a frontend developer who&rsquo;s diving deeper into APIs and backend work. I&rsquo;ve spent the last couple of years building accessible, user-friendly web applications with React, JavaScript, and TypeScript, and recently started expanding my toolkit with Python. I like tackling tough problems, collaborating with teams, and sharing what I&rsquo;ve learned. What&rsquo;s important to me is that the things I build solve real problems or bring joy to a world that can sometimes feel heavy.
-            <a href="https://resume.creddle.io/resume/dm2v090o8ru" target="_blank" rel="noopener noreferrer">View My Resume</a>
             </p>
             </section>
     )
--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -10,6 +10,7 @@ export default function Info() {
             <h1 className="name">Shavon Harris</h1>
             <h2 className="info--title">React / Javascript Developer</h2>
             <a className="info--website" href='https://shavonharris-dev.netlify.app/' target="_blank" rel="noopener noreferrer">shavonharris-dev.com</a>
+            <a href="https://resume.creddle.io/resume/dm2v090o8ru" target="_blank" rel="noopener noreferrer">View My Resume</a>
             <div className="buttonsContainer">
             <a href="mailto:shavonharris9114@gmail.com">
             <button className="button--email" href="shavonharris9114@gmail.com">Email</button>
