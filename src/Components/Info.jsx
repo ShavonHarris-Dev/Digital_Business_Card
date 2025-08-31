96a0c8e8d445bbee75f0c5b3071645d95ef1f755
Your Name <you@example.com>
2025-08-31 11:59:06 -0500

    resume link

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -10,7 +10,7 @@ export default function Info() {
             <h1 className="name">Shavon Harris</h1>
             <h2 className="info--title">React / Javascript Developer</h2>
             <a className="info--website" href='https://shavonharris-dev.netlify.app/' target="_blank" rel="noopener noreferrer">shavonharris-dev.com</a>
-            <a className="info--website" href="https://getdowntobusinesscard.netlify.app/" target="_blank" rel="noopener noreferrer">View My Resume</a>
+            <a className="info--website" href="https://tinyurl.com/rezisha" target="_blank" rel="noopener noreferrer">View My Resume</a>
             <div className="buttonsContainer">
             <a href="mailto:shavonharris9114@gmail.com">
             <button className="button--email" href="shavonharris9114@gmail.com">Email</button>
